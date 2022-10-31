5481eae99593cd6bd508f670c6d6b3dc195e3a12
sazu07 <sazushazahn0@gmail.com>
2022-10-31 21:05:03 +0600

    ES-6 practice

--- map.js
+++ map.js
@@ -24,15 +24,22 @@
 //  console.log(output);
 
 
-const numbers= [2,4,8,10,3];
-const dabul=(number)=>number*2;
-const output= numbers.map(dabul);
-console.log(output)
+// const numbers= [2,4,8,10,3];
+// const dabul=(number)=>number*2;
+// const output= numbers.map(dabul);
+// console.log(output)
 
 // function dabul(number){
 //   result= number*2;
 //   return result;
 // }
+
+const  frinds=['rahim', 'karim', 'josim']
+const item=(frind)=>frind.length;
+
+
+const output=frinds.map(item);
+console.log(output)
 
 
 
