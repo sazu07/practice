c21a3a71052819c6927a6d95b7db31ccaa98f65a
sazu07 <sazushazahn0@gmail.com>
2022-10-31 20:17:51 +0600

    ES-6 practice

---
+++ arrow-function.js
@@ -0,0 +1,25 @@
+// const multiply=(num)=>num*5
+// const result= multiply(5);
+// console.log(result)
+// const car = {
+//     type:"Fiat", model:"500", color:"white",
+//     sit:{id:"222",color:"red",qty:"4"}
+// };
+
+// const result= sit.color;
+// const result=car.sit.qty;
+// console.log(result)
+
+
+// const{color, model}=car
+// console.log(color, model)
+// const{color, model}=car.sit
+//  console.log(color, model)
+
+
+// const myObject={
+//     x:10,y:12,z:13,a:20, b:40
+// }
+
+// const {x,y}={ x:10,y:12,z:13,a:20, b:40}
+// console.log(x,y);
---
+++ map.js
@@ -0,0 +1,38 @@
+// const numbers =[2,4,6,8]
+// const output=[];
+
+
+//  for (const number of numbers) {
+//     const result=number*2;
+//     output.push(result);
+//  }
+//  console.log(output);
+
+// const numbers =[2,4,6,8]
+// const output=[];
+// function dobulold(num){
+//   const result=num*2;
+//   return result;
+// }
+
+// const dodulIt=(num)=>num*2;
+
+//  for (const number of numbers) {
+//    const result= dodulIt(number);
+//     output.push(result);
+//  }
+//  console.log(output);
+
+
+const numbers= [2,4,8,10,3];
+const dabul=(number)=>number*2;
+const output= numbers.map(dabul);
+console.log(output)
+
+// function dabul(number){
+//   result= number*2;
+//   return result;
+// }
+
+
+
