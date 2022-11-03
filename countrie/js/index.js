297d8d79d267d75fe77974b791ceb0927a49cbcc
sazu07 <sazushazahn0@gmail.com>
2022-11-03 13:22:31 +0600

    last save

--- countrie/js/index.js
+++ countrie/js/index.js
@@ -10,17 +10,21 @@ loadCountrie();
 const displayCountry=countries=>{
 
     countries.forEach(country=>{
-        console.log(country);
+        // console.log(country);
         const countryContainer=document.getElementById('country-list');
         const div=document.createElement('div');
+        div.classList.add('demo')
+        div.innerHTML=`<h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="countryByName('banglades')">Details</button>
+        `
         countryContainer.appendChild(div);
-        const h3=document.createElement('h3');
-         h3.innerText=country.name;
-         div.appendChild(h3);
-         const p =document.createElement('p');
-         p.innerText=country.capital;
-         div.appendChild(p);
+
          
     })
 
+}
+
+const countryByName=(name)=>{
+    console.log(name)
 }
